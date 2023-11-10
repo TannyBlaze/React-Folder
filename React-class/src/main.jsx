340543930c5b151d46658c8fe2e310a9ccc88f4b
Tannyblaze <tanimolatoyeseiyiola@gmail.com>
2023-11-10 13:16:17 +0100

    second commit

--- React-class/src/main.jsx
+++ React-class/src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from 'react-redux'
+import counter from './redux/Counter.jsx'
+
+let store = configureStore({
+  reducer: {
+    counterReducer: counter,
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>,
 )
--- React-class/src/pages/Home.jsx
+++ React-class/src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement } from '../redux/Counter'
 
 function Home() {
+  const count = useSelector((state) => state.counterReducer.count)
+  console.log(count);
+  const dispatch = useDispatch()
   return (
-    <div>Tiani Peace</div>
+    <div>
+      <div>
+        <h1>{count}</h1>
+        <button className='btn btn-success' onClick={() => dispatch(increment())}>Increment</button>
+        <button className='btn btn-danger' onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+    
   )
 }
 
